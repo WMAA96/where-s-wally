0a47da8c78c99ac026b732d26c660bc53cd35359
Ryan <ryanrana_4@hotmail.com>
2022-05-20 02:24:45 +0100

    Finished basic front end - user can click on image and select who they've found

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import "./App.css";
+import Header from "./Components/Header";
+import background from "./Assets/bg1.jpeg";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [userclick, setUserclick] = useState(false);
+
+  const [x, setX] = useState();
+
+  const [y, setY] = useState();
+
+  const clicked = e => {
+    setX(e.pageX);
+    setY(e.pageY);
+    setUserclick(!userclick);
+  };
+
+  const selected = e => {
+    console.log(e.target.value);
+    setUserclick(!userclick);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="Main">
+        <div>
+          <img className="wally" src={background} alt="sdf" onClick={clicked} />
+          {userclick ? (
+            <div style={{ left: x, top: y }} className="dropdown">
+              <option onClick={selected} value="wally">
+                Wally
+              </option>
+              <option onClick={selected} value="Odlaw">
+                Odlaw
+              </option>
+              <option onClick={selected} value="Wizard">
+                Wizard
+              </option>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
