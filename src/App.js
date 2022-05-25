34d36925f48d75f599a9810faa53519fec872edf
Ryan <ryanrana_4@hotmail.com>
2022-05-25 02:57:57 +0100

    Added firebase snapshot, resized image/layout of page and added x,y coords working with resized screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import "./App.css";
 import Header from "./Components/Header";
 import background from "./Assets/bg1.jpeg";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import db from "./firebase";
+import { collection, doc, onSnapshot } from "firebase/firestore";
 
 function App() {
+  const ref = useRef(null);
   const [userclick, setUserclick] = useState(false);
 
   const [x, setX] = useState();
 
   const [y, setY] = useState();
 
+  const [char, setChar] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(collection(db, "Characters"), snapshot => {
+      setChar(snapshot.docs.map(doc => doc.data()));
+    });
+    return unsub;
+  }, []);
+
+  useEffect(() => {
+    console.log(x / ref.current.offsetWidth);
+    console.log(y / ref.current.offsetHeight);
+  }, [x]);
+
   const clicked = e => {
     setX(e.pageX);
     setY(e.pageY);
+
     setUserclick(!userclick);
   };
 
@@ -25,8 +43,14 @@ function App() {
     <div className="App">
       <Header />
       <div className="Main">
-        <div>
-          <img className="wally" src={background} alt="sdf" onClick={clicked} />
+        <div className="imageContainer">
+          <img
+            ref={ref}
+            className="wally"
+            src={background}
+            alt="sdf"
+            onClick={clicked}
+          />
           {userclick ? (
             <div style={{ left: x, top: y }} className="dropdown">
               <option onClick={selected} value="wally">
